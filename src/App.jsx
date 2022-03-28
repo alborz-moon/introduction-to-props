10a40b6d5aaf1756ea716856a0f80630d61f17ce
alborz-moon <alborzmoon@gmail.com>
2022-03-28 15:47:28 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from './Welcome'
+
+function App() {
+  return (
+    <>
+      <Welcome firstName="Abbas" lastName="Bouazar" />
+      <Welcome firstName="Alborz" lastName="mooon" />
+      <Welcome firstName="Ali" lastName="yosfi" />
+    </>
+  )
+}
+
+export default App
---
+++ src/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome(props) {
+  return (
+    <h2>
+      Hello {props.firstName} {props.lastName}!
+    </h2>
+  )
+}
+
+export default Welcome
